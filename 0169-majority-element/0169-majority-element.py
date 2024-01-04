3a21fed75138d014cca76a3303159de7f8c169d3
Rupam Ghosh <rupamghosh90693@gmail.com>
2024-01-04 09:33:10 +0530

    Time: 119 ms (99.80%), Space: 18.7 MB (21.66%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        nums.sort()
+        
+        length = len(nums)
+        
+        return nums[length//2]
+        
